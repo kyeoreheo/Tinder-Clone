4f94d760ee17aa8bf267caa86a5b8af993ef9077
Kyeore Heo <91kyoheo@gmail.com>
2021-01-22 00:44:00 -0500

    basic setup

--- Tinder Clone/Controller/HomeVC.swift
+++ Tinder Clone/Controller/HomeVC.swift
@@ -6,11 +6,26 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeVC: UIViewController {
+    // MARK:- Properties
+    private let topStack = HomeNavigationStackView()
+    
+    // MARK:- Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .red
+        configureUI()
+    }
+    
+    // MARK:- Configure
+    private func configureUI() {
+        view.backgroundColor = .white
+
+        view.addSubview(topStack)
+        topStack.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.left.right.equalToSuperview()
+        }
     }
 }
