2e2b64bb024e81c643a40bb4a82f624c03e3755a
Kyeore Heo <91kyoheo@gmail.com>
2021-01-21 21:18:16 -0500

    included pod

--- Tinder Clone/Controller/HomeVC.swift
+++ Tinder Clone/Controller/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  Tinder Clone
 //
 //  Created by Kyo on 1/21/21.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
-
-
 }
-
---
+++ Tinder Clone/Units/Extensions.swift
@@ -0,0 +1,29 @@
+//
+//  Extensions.swift
+//  Tinder Clone
+//
+//  Created by Kyo on 1/21/21.
+//
+
+import UIKit
+
+public struct AnchoredConstraints {
+    public var top, leading, bottom, trailing, width, height: NSLayoutConstraint?
+}
+
+extension UIColor {
+    static let barDeselectedColor = UIColor(white: 0, alpha: 0.1)
+}
+
+extension UIViewController {
+    func configureGradientLayer() {
+        let topColor = #colorLiteral(red: 0.9921568627, green: 0.3568627451, blue: 0.3725490196, alpha: 1)
+        let bottomColor = #colorLiteral(red: 0.8980392157, green: 0, blue: 0.4470588235, alpha: 1)
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.locations = [0, 1]
+        view.layer.addSublayer(gradientLayer)
+        gradientLayer.frame = view.frame
+    }
+}
